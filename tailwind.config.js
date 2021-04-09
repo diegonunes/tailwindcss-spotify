828f4dc94dc40620c75a968cc40f983f39cb5e4e
Diego Nunes <diegonunesdev@gmail.com>
2021-04-08 22:59:51 -0300

    🎉 initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,36 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      spacing: {
+        14: '3.5rem',
+        '125px': '125px',
+      },
+      colors: {
+        hoverspt: '#18D760',
+        'purple-main': '#2d46b9',
+        'green-main': '#1ed760',
+      },
+      backgroundImage: (theme) => ({
+        'spotify-theme': "url('../img/bursts.svg')",
+        'spotify-theme-mobile': "url('../img/bursts-mobile.svg')",
+      }),
+      backgroundSize: {
+        '175%': '175%',
+        '195%': '195%',
+      },
+      backgroundPosition: {
+        banner: '46% 4%',
+        'banner-mobile': 'top 25% center',
+      },
+      fontSize: {
+        '9xl': '9rem',
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
